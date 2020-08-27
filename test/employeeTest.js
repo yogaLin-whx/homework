ae996acd78bd3d717b72cf99ded10ce9354a99ca
WUCH8 <944204910@qq.com>
2020-08-27 21:22:25 +0800

    [homework][chris]test:testValidateTypeIfTypeIsNotContain

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -5,3 +5,14 @@ employeeTest('testValidateTypeIfTypeIsContain',t => {
   let employee = new Employee('chris','engineer');
   t.is(employee.validateType('engineer'),'')
 })
+
+employeeTest('testValidateTypeIfTypeIsNotContain',t => {
+  let employee = new Employee('chris','engineer');
+
+  try{
+    const result = employee.validateType('A');
+    t.fail();
+  }catch(e){A
+    t.is(e.message, 'Employee cannot be of type ');
+  }
+})
