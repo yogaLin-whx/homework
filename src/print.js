de553342d5016ba44f4baeb086ac2399f49d906e
WUCH8 <944204910@qq.com>
2020-08-27 21:02:40 +0800

    [homework][chris]refactor:printOwing,extract printOnConsole

--- src/print.js
+++ src/print.js
@@ -1,24 +1,24 @@
 function printOwing (invoice) {
   let outstanding = 0;
-  let print = '***********************\n'+
-              '**** Customer Owes ****\n'+
-              '***********************';
-  // calculate outstanding
   for (const o of invoice.borderSpacing) {
     outstanding += o.amount;
   }
-
-  // record due date
   const today = new Date();
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+  return printOnConsole({customer:invoice.customer,outstanding:outstanding,date:invoice.dueDate.toLocaleDateString()});
+}
 
-  // print details
-  print += `name: ${invoice.customer}\n`;
-  print += `amount: ${outstanding}\n`;
-  print += `amount: ${invoice.dueDate.toLocaleDateString()}\n`;
+function printOnConsole(data){
+  let print = '***********************\n'+
+              '**** Customer Owes ****\n'+
+              '***********************';
+  print += `name: ${data.customer}\n`;
+  print += `amount: ${data.outstanding}\n`;
+  print += `amount: ${data.date}\n`;
   console.log(print);
   return print;
 }
+
 
 module.exports = {
   printOwing
