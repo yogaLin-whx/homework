c0601229a1c10e43b8be613cbfd11457e013bfb1
WUCH8 <944204910@qq.com>
2020-08-27 21:07:58 +0800

    [homework][chris]refactor:deliveryDate,simplify if-else

--- src/delivery.js
+++ src/delivery.js
@@ -1,40 +1,18 @@
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    }
-    else if ([
-      'NY',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else {
-      deliveryTime = 3;
+    switch(anOrder.deliveryState){
+      case 'MA' || 'CT':deliveryTime = 1;break;
+      case 'NY' || 'NH':deliveryTime = 2;break;
+      default:
+          deliveryTime = 3;
     }
     return anOrder.placedOn.plusDays(1 + deliveryTime);
-  }
-  else {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
+  }else {
+    switch(anOrder.deliveryState){
+      case 'MA' || 'CT' || 'NY':deliveryTime = 2;break;
+      case 'ME' || 'NH':deliveryTime = 3;break;
+      default:
+          deliveryTime = 4;
     }
     return anOrder.placedOn.plusDays(2 + deliveryTime);
   }
